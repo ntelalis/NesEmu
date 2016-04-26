8fcddf490a2550ba8f030c089474457c7772d4cf
ntelalis <Desktop@DESKTOP-JM7VF65>
2016-04-26 23:53:23 +0300

    More cpu stuff

--- NesEmu/CPU.cpp
+++ NesEmu/CPU.cpp
@@ -1,7 +1,7 @@
 #include "CPU.h"
 
 CPU::CPU() {
-	
+	cycles = 0;
 }
 
 void CPU::Init(){
@@ -25,58 +25,188 @@ void CPU::Reset(){
 
 void CPU::Cycle(){
 	opcode = ram.Read(PC);
-	switch (opcode) {
+	switch (opcode) {	
+	
+	//LDA Instructions
 	case 0xA9:
 		LDA(Immediate());
+		PC += 2;
+		cycles += 2;
+		break;
+	case 0xA5:
+		LDA(ZeroPage());
+		PC += 2;
+		cycles += 3;
+		break;
+	case 0xB5:
+		LDA(ZeroPageX());
+		PC += 2;
+		cycles += 4;
+		break;
+	case 0xAD:
+		LDA(Absolute());
+		PC += 3;
+		cycles += 4;
+		break;
+	case 0xBD:
+		LDA(AbsoluteX());
+		PC += 3;
+		//+1 if pages crossed implement
+		cycles += 4;
+		break;
+	case 0xB9:
+		LDA(AbsoluteY());
+		PC += 3;
+		//+1 if pages crossed implement
+		cycles += 4;
+		break;
+	case 0xA1:
+		LDA(IndirectX());
+		PC += 2;
+		cycles += 6;
+		break;
+	case 0xB1:
+		LDA(IndirectY());
+		PC += 2;
+		//+1 if pages crossed implement
+		cycles += 5;
+		break;
+	//LDX Instructions
+	case 0xA2:
+		LDX(Immediate());
+		PC += 2;
+		cycles += 2;
+		break;
+	case 0xA6:
+		LDX(ZeroPage());
+		PC += 2;
+		cycles += 3;
+		break;
+	case 0xB6:
+		LDX(ZeroPageY());
+		PC += 2;
+		cycles += 4;
+		break;
+	case 0xAE:
+		LDX(Absolute());
+		PC += 3;
+		cycles += 4;
+		break;
+	case 0xBE:
+		LDX(AbsoluteY());
+		PC += 3;
+		//+1 if pages crossed implement
+		cycles += 4;
+		break;
+	//LDY Instructions
+	case 0xA0:
+		LDY(Immediate());
+		PC += 2;
+		cycles += 2;
+		break;
+	case 0xA4:
+		LDY(ZeroPage());
+		PC += 2;
+		cycles += 3;
+		break;
+	case 0xB4:
+		LDY(ZeroPageX());
+		PC += 2;
+		cycles += 4;
+		break;
+	case 0xAC:
+		LDY(Absolute());
+		PC += 3;
+		cycles += 4;
+		break;
+	case 0xBC:
+		LDX(AbsoluteX());
+		PC += 3;
+		//+1 if pages crossed implement
+		cycles += 4;
+		break;
 	}
 }
 
-void CPU::LDA() {
-	if (ram.Read(PC + 1) < 0x80)
-		PS &= 0b11111110;
-	else
-		PS |= 0b00000001;
-	if (ram.Read(PC + 1) == 0x0)
-		PS |= 0b01000000;
-	else
-		PS &= 0b10111111;
+// Set/Unset & Read Processor Status Register bits
 
+void CPU::SetZ(uint8_t value) {
+	if (value == 0x0)
+		PS |= 1 << 7;
+	else
+		PS &= ~(1 << 7);
 }
 
-uint16_t CPU::read16(uint16_t address) {
-	return (uint16_t)ram.Read(address + 1) << 8 | (uint16_t)ram.Read(address);
+void CPU::SetN(uint8_t value) {
+	if (value & 0x80 == 0)
+		PS &= ~(1 << 1);
+	else
+		PS |= 1 << 1;
 }
 
-uint8_t CPU::Implied(){
-	return 0x0;
+bool CPU::GetBit(uint8_t value, int bit) {
+	return (value & (1 << bit)) != 0;
+}
+
+// Instructions
+void CPU::LDA(uint16_t address) {
+	A = ram.Read(address);
+	CPU::SetN(A);
+	CPU::SetZ(A);
+}
+
+void CPU::LDX(uint16_t address) {
+	X = ram.Read(address);
+	CPU::SetN(X);
+	CPU::SetZ(X);
+}
+
+void CPU::LDY(uint16_t address) {
+	Y = ram.Read(address);
+	CPU::SetN(Y);
+	CPU::SetZ(Y);
+}
+
+void CPU::ADC(uint16_t address) {
+	// I am here
+	A += (int8_t)ram.Read(address) + GetBit(PS,CF);
+	CPU::SetZ(A);
+	CPU::SetZ(A);
+}
+
+// Addressing Modes
+
+uint16_t CPU::Implied(){
+	return 0;
+}
+
+uint16_t CPU::Accumulator() {
+	return 0;
 }
 
 uint16_t CPU::Immediate(){
 	return PC + 1;
 }
 
-uint8_t	CPU::Relative(){
-	uint16_t tempAddr = ram.Read(PC + 1);
+uint16_t CPU::ZeroPage(){
+	return (uint16_t)(ram.Read(PC + 1) & 0x00FF);
+}
+
+uint16_t CPU::ZeroPageX(){
+	return (uint16_t)((ram.Read(PC + 1) + X) & 0x00FF);
+}
+
+uint16_t CPU::ZeroPageY(){
+	return (uint16_t)((ram.Read(PC + 1) + Y) & 0x00FF);
+}
+
+uint16_t CPU::Relative() {
+	//Not sure if its ok
+	uint16_t tempAddr = (uint16_t)(ram.Read(PC + 1));
 	if (tempAddr < 0x80)
 		return PC + 2 + tempAddr;
 	else
 		return PC + 2 + tempAddr - 0x100;
-}
-
-uint8_t	CPU::Accumulator(){
-	return 0x0;
-}
-
-uint8_t CPU::ZeroPage(){
-	return ram.Read(PC + 1) & 0xFF;
-}
-
-uint8_t CPU::ZeroPageX(){
-	return (ram.Read(PC + 1) + X) & 0xFF;
-}
-
-uint8_t CPU::ZeroPageY(){
-	return (ram.Read(PC + 1) + Y) & 0xFF;
 }
 
 uint16_t CPU::Absolute(){
@@ -94,6 +224,7 @@ uint16_t CPU::AbsoluteY(){
 uint16_t CPU::Indirect(){
 	uint16_t tempAddr = read16(PC + 1);
 	//Wrap JMP Bug
+	//Not sure if bug implementation is ok
 	if ((tempAddr & 0xFF) == 0xFF)
 		return (uint16_t)ram.Read(tempAddr & 0xFF00) << 8 | (uint16_t)ram.Read(tempAddr);
 	else
@@ -102,10 +233,13 @@ uint16_t CPU::Indirect(){
 
 uint16_t CPU::IndirectX(){
 	return read16((ram.Read(PC + 1) + X) & 0xFF);
-	//Bug Implementation?
 }
 
 uint16_t CPU::IndirectY(){
 	return read16(ram.Read(PC + 1)) + Y;
-	//Bug Implementation?
+}
+
+// Helper function that returns an address from 2 consecutive memory addresses (Little Endian)
+uint16_t CPU::read16(uint16_t address) {
+	return (uint16_t)ram.Read(address + 1) << 8 | (uint16_t)ram.Read(address);
 }
--- NesEmu/CPU.h
+++ NesEmu/CPU.h
@@ -3,17 +3,28 @@
 #include <stdint.h>
 #include "CPUMemoryMap.h"
 
+//Carry, Zero, Interrupt, Decimal, Break, Unused, Overflow, Negative flags 1-bit
+#define CF 8
+#define ZF 7
+#define IF 6
+#define DF 5
+#define BF 4
+#define UF 3
+#define VF 2
+#define NF 1
+
 class CPU {
 private:
 
 	RAM ram;
 
+	//Registers
 	uint16_t PC;							//Program counter 16-bit
 	uint8_t SP, A, X, Y, PS;				//Stack Pointer,Acumulator,X & Y Register, Processor Status Register 8-bit
-	bool CF, ZF, IF, DF, BF, UF, VF, NF;	//Carry, Zero, Interrupt, Decimal, Break, Unused, Overflow, Negative flags 1-bit
 
-
-	int8_t opcode;
+	//Counters
+	long int cycles;
+	uint8_t opcode;
 
 	uint16_t read16(uint16_t address);
 public:
@@ -22,16 +33,26 @@ public:
 	void Reset();
 	void Cycle();
 
-	void LDA();
+	void SetZ(uint8_t value);
+	void SetN(uint8_t value);
+	bool GetBit(uint8_t value, int bit);
+
+	void LDA(uint16_t address);
+	void LDX(uint16_t address);
+	void LDY(uint16_t address);
+	void ADC(uint16_t address);
+
+	
+	
 
 	//Addressing modes
-	uint8_t Implied();
+	uint16_t Implied();
 	uint16_t Immediate();
-	uint8_t Relative();
-	uint8_t Accumulator();
-	uint8_t ZeroPage();
-	uint8_t ZeroPageX();
-	uint8_t ZeroPageY();
+	uint16_t Relative();
+	uint16_t Accumulator();
+	uint16_t ZeroPage();
+	uint16_t ZeroPageX();
+	uint16_t ZeroPageY();
 	uint16_t Absolute();
 	uint16_t AbsoluteX();
 	uint16_t AbsoluteY();
